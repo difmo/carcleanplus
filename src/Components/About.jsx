f5542f5797107e2e05cd16352997323f6695a7d6
amarcodeservir <amarcodeservir@gmail.com>
2024-11-30 10:24:21 +0530

    about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,38 +7,35 @@ import HeaderCard from "./Cardheader";
 const About = () => {
   return (
     <>
-      <div  id="about"
+      <div
+        id="about"
         className="section bg-no-repeat bg-cover bg-center"
         style={{
           backgroundImage: `url(${img})`,
         }}
-      >     
-
+      >
         <div className="bg-cover bg-center bg-black bg-opacity-80">
           <div className="justify-center">
-          <div className=" font-mono text-2xl md:text-3xl py-5 text-mywhite  flex justify-center">
-            About Us
-          </div>
-          <div className="sm:px-5 md:flex md:mx-0">
-            <div className=" lg:w-1/2 self-center mb-5 md:mb-0 w-full h-full  ">
-              <img src={car} alt="Car" className="object-cover " />
+            <div className=" font-mono text-2xl md:text-3xl py-5 text-mywhite  flex justify-center">
+              About Us
             </div>
-            <div className="w-full lg:w-1/2 md:mx-1 md:pb-5">
-              <p className="font-sans text-lg md:text-xl text-white  px-3 ">
-                A clean car is a happy car. Who doesn't want to drive a clean
-                car? At DAILY CARWASH, we aim to change the previous car washing
-                concept where car owners had to deal with inconvenient,
-                untrained, and unprofessional people to get their prized
-                possession clean and protected. Daily Carwash is a
-                ground-breaking step in the car cleaning sector, providing the
-                best car cleaning service at your doorstep with the right
-                equipment by highly trained professionals.
-              </p>
-              <div className="py-4 px-20" >
-                <Button />
+            <div className="sm:px-5 md:flex md:mx-0">
+              <div className=" lg:w-1/2 self-center mb-5 md:mb-0 w-full h-full  ">
+                <img src={car} alt="Car" className="object-cover " />
+              </div>
+              <div className="w-full lg:w-1/2 md:mx-1 md:pb-5">
+                <p className="font-sans text-lg md:text-xl text-white  px-3 ">
+                  CarCleanPlus transforms car cleaning with vocal washing
+                  technology, allowing you to schedule, customize, and track
+                  washes with just your voice. Whether it is a quick refresh or
+                  a thorough cleaning, our hands-free service ensures that your
+                  vehicle stays in pristine condition effortlessly and reliably.
+                </p>
+                <div className="py-4 px-20">
+                  <Button />
+                </div>
               </div>
             </div>
-          </div>
           </div>
         </div>
       </div>
