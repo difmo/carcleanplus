5dff9f6361de941d97323cbb57846df96df77d02
AmarMauryaDeveloper <amarmaurya296@gamil.com>
2025-02-18 18:35:43 +0530

    push

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -3,7 +3,10 @@ const Button = () => {
   return (
     <>
       <div className="bg-primary  text-mywhite  rounded-lg text-center  px-9  font-bold border-2 border-indigo-50 hover:bg-myyellow hover:text-black">
-        <button className="w-28 h-10">Discover More</button>
+        <a href="https://play.google.com/store/apps/details?id=com.codeservir.carcleanplus.new">
+          {" "}
+          <button className="w-28 h-10">Discover More</button>
+        </a>
       </div>
     </>
   );
